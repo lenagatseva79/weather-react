a4287affff2bf0c811d98cc2c91f0827c36520d4
lenagatseva79 <lenakassem79@gmail.com>
2022-10-29 19:53:41 +0300

    Added footer with a link to GitHub

--- src/App.js
+++ src/App.js
@@ -9,6 +9,17 @@ function App() {
         <h1>Weather App</h1>
         <Weather city="Odesa" />
       </header>
+      <div className="footer">
+        <a
+          className="link"
+          href="https://github.com/lenagatseva79/weather-react"
+          alt="GitHub Link"
+          target="blank"
+        >
+          Open-source code
+        </a>
+        , by Lena Gatseva
+      </div>
     </div>
   );
 }
